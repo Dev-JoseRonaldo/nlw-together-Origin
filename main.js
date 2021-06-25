e1e5eae9804e8da904a687dbfa0c1a3f716919ef
José Ronaldo <dev.joseronaldo@gmail.com>
2021-06-24 22:54:42 -0300

    menu mobile, about e services

---
+++ main.js
@@ -0,0 +1,30 @@
+/* abre e fecha o menu quando clica no icone */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* quando cliclar em um item do menu , esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* mudar o header da pagina quando der o scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
