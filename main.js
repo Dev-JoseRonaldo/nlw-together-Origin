56476871a13dca6b2df88c90925ab39ae6b793a2
José Ronaldo <dev.joseronaldo@gmail.com>
2021-06-26 23:54:34 -0300

    footer and button back to top

--- main.js
+++ main.js
@@ -18,16 +18,17 @@ for (const link of links) {
 }
 
 /* mudar o header da pagina quando der o scroll */
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
+
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
     header.classList.remove('scroll')
   }
-})
+}
 
 const swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
@@ -51,7 +52,30 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials
-  #contact .text, #contact .links
+  #contact .text, #contact .links,
+  footer .brand, footer .social
   `,
   { interval: 100 }
 )
+
+/*Botão voltar ao top */
+
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+  if (this.window.scrollY > 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+  if (this.window.scrollY > 3700) {
+    backToTopButton.classList.add('back-to-top-alt')
+  } else {
+    backToTopButton.classList.remove('back-to-top-alt')
+  }
+}
+
+/* When Scroll */
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+})
